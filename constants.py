c159cacfb683034839f09d6f243a0d467fd52c51
Isaiah Chiraira <itchiraira@gmail.com>
2023-11-02 08:53:34 +0200

    Initial commit

---
+++ constants.py
@@ -0,0 +1,3 @@
+# Replace with your own OpenAI API Key https://platform.openai.com/account/api-keys
+# and rename this file to constants.py.
+APIKEY = "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"
